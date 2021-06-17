b015190640c77ec09b6b73f012b776a52be9f33f
Adeel <arehman@acrobits.cz>
2021-06-17 20:11:02 +0500

    checksum updated

--- Package.swift
+++ Package.swift
@@ -15,10 +15,10 @@ let package = Package(
         .binaryTarget(
             name: "Softphone",
             url: "https://dist.acrobits.net/saas-swift-package/debug/Softphone.xcframework-153555.zip",
-            checksum: ""),
+            checksum: "e509c3e42a4995248f2eaf1bd856092d29b5f616dacd2ce9f2b5199057213a93"),
         .binaryTarget(
             name: "Softphone_Swift",
             url: "https://dist.acrobits.net/saas-swift-package/debug/Softphone_Swift.xcframework-153555.zip",
-            checksum: ""),
+            checksum: "eb355b7f7d7d0c6f0ac0bc2fbead7c38ae5694d7faee9c8dff6f85490bd77769"),
     ]
 )
