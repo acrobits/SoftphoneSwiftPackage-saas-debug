96299d9da11d80b3729f7191c69f46029eef1ca1
Stanislav Kutil <skutil@acrobits.cz>
2022-01-17 20:37:59 +0100

    Package updated to 170885

--- Package.swift
+++ Package.swift
@@ -14,11 +14,11 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Softphone",
-            url: "https://dist.acrobits.net/saas-swift-package/debug/Softphone.xcframework-169629.zip",
-            checksum: "7d7eddbf8337bc0c4493cf8ad173392b5f475dfb394141785751e2898011da11"),
+            url: "https://dist.acrobits.net/saas-swift-package/debug/Softphone.xcframework-170885.zip",
+            checksum: "f45dfcaa14de6fc99c818d2101c0c1ae20ac066cc02b86c7bab8cfabba2afaf9"),
         .binaryTarget(
             name: "Softphone_Swift",
-            url: "https://dist.acrobits.net/saas-swift-package/debug/Softphone_Swift.xcframework-169629.zip",
-            checksum: "5331e231db8017752d1f9c36c0dc22d9fb5e01a27144c317ce6621a901079e04"),
+            url: "https://dist.acrobits.net/saas-swift-package/debug/Softphone_Swift.xcframework-170885.zip",
+            checksum: "8ede274c5998930a56519721d58b755344418465791bd9278c18e39f29d40753"),
     ]
 )
