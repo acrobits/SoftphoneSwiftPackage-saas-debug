08c0554e487d51d2520fa340497eaf738dad27de
autobuild <autobuild@m2mini.inner.acrobits.cz>
2024-01-15 11:25:08 +0100

    Package updated to 1.1.42

--- Package.swift
+++ Package.swift
@@ -14,11 +14,11 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Softphone",
-            url: "https://dist.acrobits.net/saas-swift-package/debug/Softphone.xcframework-1.1.41.zip",
-            checksum: "2ff23eb3d09d515469dde832a48c950f71e3aef2089460c737304f2a80dc09b2"),
+            url: "https://dist.acrobits.net/saas-swift-package/debug/Softphone.xcframework-1.1.42.zip",
+            checksum: "ef1d22363a38d92841b953cf62c297b3fd2fb061060a49d508f3302dbf3436d6"),
         .binaryTarget(
             name: "Softphone_Swift",
-            url: "https://dist.acrobits.net/saas-swift-package/debug/Softphone_Swift.xcframework-1.1.41.zip",
-            checksum: "400621dae1cdd4bc06c56e0e0b328fcc991695c623fb4e027c0fc8d2fc201d0a"),
+            url: "https://dist.acrobits.net/saas-swift-package/debug/Softphone_Swift.xcframework-1.1.42.zip",
+            checksum: "eb80a3dc06063fa0b4be58f9983f4c5ac187b3ac48078423ef0b94d46e2674f7"),
     ]
 )
